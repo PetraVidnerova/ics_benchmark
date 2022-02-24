f7e906ff638de1cd4d50a289c62ec403ae886293
Petra Vidnerova <petra.vidnerova@gmail.com>
2022-02-24 14:06:20 +0000

    rewritten

--- benchmark2.py
+++ benchmark2.py
@@ -2,9 +2,8 @@ import click
 import io
 import os
 import shutil
-from contextlib import contextmanager
-from typing import List, Tuple
 
+import tqdm
 import numpy
 import torch
 import torch.backends.cudnn as cudnn
@@ -23,6 +22,27 @@ from torch.optim import SGD
 from torch.utils.data import DataLoader
 
 
+def work(model, data_loader, epochs, device):
+    for epoch in range(epochs):
+        data_loader.batch_sampler.sampler.set_epoch(epoch)
+
+        model.train()
+        
+        with tqdm(total=len(data_loader)) as t:
+            for i, (images, target) in enumerate(data_loader):
+
+                images = images.cuda(device, non_blocking=True)
+                target = target.cuda(device, non_blocking=True)
+                
+                output = model(images)
+                loss = criterion(output, target)
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                t.update()
+
+    
 @click.command()
 @click.option("--data_root", default="./data/")
 @click.option("--workers", default=1) 
@@ -31,8 +51,6 @@ from torch.utils.data import DataLoader
 @click.option("--lr", default=1e-3)
 @click.option("--momentum", default=0.9) 
 @click.option("--weight_decay", default=1e-4) 
-
-
 def main(data_root, workers, epochs, batch_size, lr, momentum, weight_decay):
     
     device_id = int(os.environ["LOCAL_RANK"])
@@ -58,233 +76,18 @@ def main(data_root, workers, epochs, batch_size, lr, momentum, weight_decay):
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
-    dataset = datasets.ImageFolder(
-        traindir,
-        ,
-    )
-    train_sampler = ElasticDistributedSampler(train_dataset)
-    train_loader = DataLoader(
-        train_dataset,
-        batch_size=batch_size,
-        num_workers=num_data_workers,
-        pin_memory=True,
-        sampler=train_sampler,
-    )
-    val_loader = DataLoader(
-        datasets.ImageFolder(
-            valdir,
-            transforms.Compose(
-                [
-                    transforms.Resize(256),
-                    transforms.CenterCrop(224),
-                    transforms.ToTensor(),
-                    normalize,
-                ]
-            ),
-        ),
-        batch_size=batch_size,
-        shuffle=False,
-        num_workers=num_data_workers,
-        pin_memory=True,
-    )
-    return train_loader, val_loader
-
+    is_valid = lambda x: True if x.endswith(".JPEG") else False
+    print("Prepare dataset ... ", end="", flush=True)
+    dataset = torchvision.datasets.ImageFolder(data_root, transform=transform,
+                                               is_valid_file=is_valid)
+    data_sampler = ElasticDistributedSampler(dataset)
+    data_loader = DataLoader(dataset, batch_size=batch_size, num_workers=num_data_workers,
+                             pin_memory=True, sampler=data_sampler)
+    print("ok", flush=True)
     
-
-    start_epoch = state.epoch + 1
-    print(f"=> start_epoch: {start_epoch}, best_acc1: {state.best_acc1}")
-
-    print_freq = args.print_freq
-    for epoch in range(start_epoch, args.epochs):
-        state.epoch = epoch
-        train_loader.batch_sampler.sampler.set_epoch(epoch)
-        adjust_learning_rate(optimizer, epoch, args.lr)
-
-        # train for one epoch
-        train(train_loader, model, criterion, optimizer, epoch, device_id, print_freq)
-
-        # evaluate on validation set
-        acc1 = validate(val_loader, model, criterion, device_id, print_freq)
-
-        # remember best acc@1 and save checkpoint
-        is_best = acc1 > state.best_acc1
-        state.best_acc1 = max(acc1, state.best_acc1)
+    work(model, data_loader, epochs, device_id)
 
 
-
-class State:
-
-    def __init__(self, arch, model, optimizer):
-        self.epoch = -1
-        self.best_acc1 = 0
-        self.arch = arch
-        self.model = model
-        self.optimizer = optimizer
-
-    def capture_snapshot(self):
-        return {
-            "epoch": self.epoch,
-            "best_acc1": self.best_acc1,
-            "arch": self.arch,
-            "state_dict": self.model.state_dict(),
-            "optimizer": self.optimizer.state_dict(),
-        }
-
-    def apply_snapshot(self, obj, device_id):
-
-        self.epoch = obj["epoch"]
-        self.best_acc1 = obj["best_acc1"]
-        self.state_dict = obj["state_dict"]
-        self.model.load_state_dict(obj["state_dict"])
-        self.optimizer.load_state_dict(obj["optimizer"])
-
-    def save(self, f):
-        torch.save(self.capture_snapshot(), f)
-
-    def load(self, f, device_id):
-        # Map model to be loaded to specified single gpu.
-        snapshot = torch.load(f, map_location=f"cuda:{device_id}")
-        self.apply_snapshot(snapshot, device_id)
-
-
-
-
-
-
-@contextmanager
-def tmp_process_group(backend):
-    cpu_pg = dist.new_group(backend=backend)
-    try:
-        yield cpu_pg
-    finally:
-        dist.destroy_process_group(cpu_pg)
-
-
-
-def train(
-    train_loader: DataLoader,
-    model: DistributedDataParallel,
-    criterion,  # nn.CrossEntropyLoss
-    optimizer,  # SGD,
-    epoch: int,
-    device_id: int,
-    print_freq: int,
-):
-    batch_time = AverageMeter("Time", ":6.3f")
-    data_time = AverageMeter("Data", ":6.3f")
-    losses = AverageMeter("Loss", ":.4e")
-    top1 = AverageMeter("Acc@1", ":6.2f")
-    top5 = AverageMeter("Acc@5", ":6.2f")
-    progress = ProgressMeter(
-        len(train_loader),
-        [batch_time, data_time, losses, top1, top5],
-        prefix="Epoch: [{}]".format(epoch),
-    )
-
-    # switch to train mode
-    model.train()
-
-    end = time.time()
-    for i, (images, target) in enumerate(train_loader):
-        # measure data loading time
-        data_time.update(time.time() - end)
-
-        images = images.cuda(device_id, non_blocking=True)
-        target = target.cuda(device_id, non_blocking=True)
-
-        # compute output
-        output = model(images)
-        loss = criterion(output, target)
-
-        # measure accuracy and record loss
-        acc1, acc5 = accuracy(output, target, topk=(1, 5))
-        losses.update(loss.item(), images.size(0))
-        top1.update(acc1[0], images.size(0))
-        top5.update(acc5[0], images.size(0))
-
-        # compute gradient and do SGD step
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        # measure elapsed time
-        batch_time.update(time.time() - end)
-        end = time.time()
-
-        if i % print_freq == 0:
-            progress.display(i)
-
-
-def validate(
-    val_loader: DataLoader,
-    model: DistributedDataParallel,
-    criterion,  # nn.CrossEntropyLoss
-    device_id: int,
-    print_freq: int,
-):
-    batch_time = AverageMeter("Time", ":6.3f")
-    losses = AverageMeter("Loss", ":.4e")
-    top1 = AverageMeter("Acc@1", ":6.2f")
-    top5 = AverageMeter("Acc@5", ":6.2f")
-    progress = ProgressMeter(
-        len(val_loader), [batch_time, losses, top1, top5], prefix="Test: "
-    )
-
-    # switch to evaluate mode
-    model.eval()
-
-    with torch.no_grad():
-        end = time.time()
-        for i, (images, target) in enumerate(val_loader):
-            if device_id is not None:
-                images = images.cuda(device_id, non_blocking=True)
-            target = target.cuda(device_id, non_blocking=True)
-
-            # compute output
-            output = model(images)
-            loss = criterion(output, target)
-
-            # measure accuracy and record loss
-            acc1, acc5 = accuracy(output, target, topk=(1, 5))
-            losses.update(loss.item(), images.size(0))
-            top1.update(acc1[0], images.size(0))
-            top5.update(acc5[0], images.size(0))
-
-            # measure elapsed time
-            batch_time.update(time.time() - end)
-            end = time.time()
-
-            if i % print_freq == 0:
-                progress.display(i)
-
-        # TODO: this should also be done with the ProgressMeter
-        print(
-            " * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}".format(top1=top1, top5=top5)
-        )
-
-    return top1.avg
-
-
-
-
-
-
-
-
-def accuracy(output, target, topk=(1,)):
-    with torch.no_grad():
-        maxk = max(topk)
-        batch_size = target.size(0)
-
-        _, pred = output.topk(maxk, 1, True, True)
-        pred = pred.t()
-        correct = pred.eq(target.view(1, -1).expand_as(pred))
-
-        res = []
-        for k in topk:
-            correct_k = correct[:k].reshape(1, -1).view(-1).float().sum(0, keepdim=True)
-            res.append(correct_k.mul_(100.0 / batch_size))
-        return res
 
 
 if __name__ == "__main__":
